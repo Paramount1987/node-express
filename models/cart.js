161c6737f0a5c218fb4c0f15857114ec857fa3bc
Artem <a.petrov@uplab.ru>
2019-11-15 18:20:26 +0300

    != update cart

--- models/cart.js
+++ models/cart.js
@@ -34,6 +34,32 @@ class Cart {
 		});
 	}
 
+	static async remove(id) {
+		const cart = await Cart.fetch();
+		const idx = cart.courses.findIndex(c => c.id === id);
+		const course = cart.courses[idx];
+
+		if (course.count === 1) {
+			// del
+			cart.courses = cart.courses.filter(c => c.id !== id);
+		} else {
+			// edit count
+			cart.courses[idx].count--;
+		}
+
+		cart.price -= course.price;
+
+		return new Promise((resolve, reject) => {
+			fs.writeFile(p, JSON.stringify(cart), err => {
+				if(err) {
+					reject(err);
+				} else {
+					resolve(cart);
+				}
+			});
+		});
+	}
+
 	static async fetch() {
 		return new Promise((resolve, reject) => {
 			fs.readFile(p, 'utf-8', (err, content) => {
